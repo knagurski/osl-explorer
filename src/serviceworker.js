6933613591b2d8de1e0c77dfa9b3858116a7ec56
Kevin Nagurski <knagurski@gmail.com>
2020-05-05 13:42:53 +0100

    update workbox to 5.1.3

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,5 +1,5 @@
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js"
 );
 
 const { registerRoute, setDefaultHandler } = workbox.routing;
